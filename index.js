b7790ed226ef9ea73e99db27ce78c4c950ef260b
sadekinchowdhury <rumelchow340@gmail.com>
2022-11-10 08:29:23 +0600

    am

--- index.js
+++ index.js
@@ -65,6 +65,7 @@ function run() {
         })
         app.get('/users/:id', async (req, res) => {
             const id = req.params.id;
+            console.log(id)
             const query = { _id: ObjectId(id) }
             const result = await reviewcollection.deleteOne(query)
 
