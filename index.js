82b330856e023c7c395d0d586307bc8ebcf178dd
sadekinchowdhury <rumelchow340@gmail.com>
2022-11-10 19:52:09 +0600

    delet

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const cors = require('cors')
+const cors = require('cors');
+const { json } = require('express');
 
 require('dotenv').config()
 const port = process.env.PORT || 5000
@@ -19,13 +21,41 @@ app.get('/', (req, res) => {
 
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.oq7uvoj.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+function verifyJWT(req, res, next) {
+
+    const authHeaders = req.headers.authorization
+    console.log(authHeaders)
+
+    if (!authHeaders) {
+        res.status(401).send({ massage: 'unauthoriz' })
+    }
+    const token = authHeaders.split(' ')[1];
+    jwt.verify(token, process.env.USER_KEY, function (error, decoded) {
+        if (error) {
+            res.status(401).send({ massage: 'unauthorized' })
+
+        }
+        req.decoded = decoded;
+        next()
+    })
+}
+
 function run() {
 
     try {
         const FoodCollection = client.db('foodbar').collection('food')
         const reviewcollection = client.db('revieitem').collection('review')
 
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.USER_KEY, { expiresIn: '1h' })
+            res.send({ token })
 
+
+        })
         app.get('/home', async (req, res) => {
             const query = {}
             const cursor = FoodCollection.find(query)
@@ -64,7 +94,7 @@ function run() {
             res.send(cursor)
 
         })
-        app.get('/users/:id', async (req, res) => {
+        app.delete('/users/:id', async (req, res) => {
             const id = req.params.id;
             console.log(id)
             const query = { _id: ObjectId(id) }
@@ -73,7 +103,13 @@ function run() {
             res.send(result)
 
         })
-        app.get('/users', async (req, res) => {
+        app.get('/users', verifyJWT, async (req, res) => {
+            const decoded = req.decoded;
+
+            if (decoded.email !== req.query.email) {
+
+                res.status(401).send({ massage: 'unauthorized' })
+            }
             let query = {}
             if (req.query.email) {
                 query = {
